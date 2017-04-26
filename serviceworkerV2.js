580af8b8ce8278cbd91d7f6bf8501e2a4d160465
sean stevenson <sean stevenson>
2017-04-27 00:25:42 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -203,6 +203,13 @@ self.addEventListener('activate', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
-  console.log('Fetch request for:', event.request.url);
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return new Response(
+        'Welcome to the our service worker test \n'+        
+'There seems to be a problem with your connection.\n'+        
+'We look forward to telling you about Paddy as soon as you are online'
+      );
+    })
+  );
 });
-
