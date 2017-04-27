926b7aa83d05dad9418eeaee8f2c7eaf3f48539d
sean stevenson <sean stevenson>
2017-04-27 12:50:29 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -202,7 +202,7 @@ self.addEventListener('activate', function(event) {
 });*/
 
 
-var CACHE_NAME = 'gih-cache-v4';
+/*var CACHE_NAME = 'gih-cache-v4';
 var CACHED_URLS = [
   'offline.html',
   'mystyles.css'
@@ -241,6 +241,38 @@ self.addEventListener('activate', function(event) {
           }
         })
       );
+    })
+  );
+});
+*/
+
+
+
+var CACHE_NAME = 'gih-cache';
+var CACHED_URLS = [
+  'offline.html',
+  'mystyles.css’,
+  'dino.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
     })
   );
 });
