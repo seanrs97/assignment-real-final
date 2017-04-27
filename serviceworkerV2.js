4f1a65d35570621c078526781ce3fa2f77b4cb4b
sean stevenson <sean stevenson>
2017-04-27 13:11:08 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -248,14 +248,67 @@ self.addEventListener('activate', function(event) {
 
 
 
-var CACHE_NAME = 'gih-cache-v4';
+var CACHE_NAME = 'gih-cache-v5';
 var CACHED_URLS = [
-  'offline.html',
-  'mystyles.css',
+   'index.html',
+   'staffs-uni.html',
+   'sign-up.html',
+  
+   'mystyles.css',
+   'styles.css',
+   'offline.html',
+   'images/favicon/android-icon-36x36.png',
+   'images/favicon/android-icon-48x48.png',
+   'images/favicon/android-icon-72x72.png',
+   'images/favicon/android-icon-96x96.png',
+   'images/favicon/android-icon-144x144.png',
+   'images/favicon/android-icon-192x192.png',
+   'images/favicon/favicon-16x16.png',
+   'images/favicon/favicon-32x32.png',
+   'images/favicon/favicon-96x96.png',
+   'images/favicon/ic_launcher-1x.png',
+   'images/favicon/ic_launcher-2x.png',
+   'images/favicon/ic_launcher-3x.png',
+   'images/favicon/ic_launcher-4x.png',
+   'images/favicon/ic_launcher-5x.png',
+  
+   'images/favicon/manifest.json',
+   'images/activities-image.jpg',
+   'images/banner.jpg',
+   'images/clubs-image.jpg',
+   'images/main-image.jpg',
+   'images/party-image.jpg',
+   'images/pic01.jpg',
+   'images/pic02.jpg',
+   'images/pic03.jpg',
+   'images/study-image.jpg',
+   'images/universityImage-1x.png',
+   'images/universityImage-2x.png',
+   'images/universityImage-3x.png',
+   'assets/css/images/overlay.png',
+   'assets/css/images/shadow.png',
+   'assets/css/font-awesome.min.css',
+   'assets/css/ie8.css',
+   'assets/css/main.css',
+   'assets/css/normalize.css',
+   'assets/fonts/FontAwesome.otf',
+   'assets/js/gen_validatorv31.js',
+   'assets/js/jquery.min.js',
+   'assets/js/main.js',
+   'assets/js/modernizr.js',
+   'assets/sass/ie8.scss',
+   'assets/sass/main.scss',
+   'assets/browserconfig.xml',
+   'assets/js/offline-map.js',
+   'assets/js/material.js',
+   'eventImages/event-default.png',
+   'offlinemap.jpg',
+   'events.json',
+   'scripts.js'
 ];
 
-
 self.addEventListener('install', function(event) {
+  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
@@ -270,12 +323,27 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('offline.html');
+          return caches.match('first.html');
         }
       });
     })
   );
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
