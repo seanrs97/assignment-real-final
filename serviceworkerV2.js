9f85205ff9c96f5f84cc9c5759240769bc7f32cb
sean stevenson <sean stevenson>
2017-04-27 22:48:25 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -248,8 +248,8 @@ self.addEventListener('activate', function(event) {
 
 
 var TEMP_IMAGE_CACHE_NAME = 'temp-cache-v7';
-var BASE_PATH = 'assignment-real-final/';
-var CACHE_NAME = 'gih-cache-v6';
+var BASE_PATH = '/assignment-real-final/';
+var CACHE_NAME = 'gih-cache-v4';
 var newsAPIJSON = "https://newsapi.org/v1/articles?source=bbc-news&apiKey=a0a4a38847b64cf1b96a92066e7933af";
 
 var CACHED_URLS = [
