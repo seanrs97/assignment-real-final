0e4bf39881a22e5fc522aee85c49dfddf2b14d30
sean stevenson <sean stevenson>
2017-04-28 00:24:11 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -245,27 +245,17 @@ self.addEventListener('activate', function(event) {
   );
 });
 */
-
-
-var TEMP_IMAGE_CACHE_NAME = 'temp-cache-v7';
-var BASE_PATH = '/assignment-real-final/';
-var CACHE_NAME = 'gih-cache-v6';
-//var newsAPIJSON = "https://newsapi.org/v1/articles?source=bbc-news&apiKey=a0a4a38847b64cf1b96a92066e7933af";
-
+var CACHE_NAME = 'gih-cache-v5';
 var CACHED_URLS = [
-	
-   // BASE DIRECTORY
-   BASE_PATH + 'events.json',
-   BASE_PATH + 'index.html',
-   BASE_PATH + 'staffs-uni.html'
-
-   
+  // Our HTML
+  'index.html',
+  // Stylesheets and fonts
+  // JavaScript
+  // Images
 ];
 
-var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBMA8ic4_Gyh0D7XUW9gBtrCcEwoBbBJ8E&callback=initMap';
-
 self.addEventListener('install', function(event) {
-  // Cache everything in CACHED_URLS. Installation fails if anything fails to cache
+  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
@@ -274,113 +264,18 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  var requestURL = new URL(event.request.url);
-  // Handle requests for index.html
-  if (requestURL.pathname === BASE_PATH + 'index.html') {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match('index.html').then(function(cachedResponse) {
-          var fetchPromise = fetch('index.html').then(function(networkResponse) {
-            cache.put('index.html', networkResponse.clone());
-            return networkResponse;
-          });
-          return cachedResponse || fetchPromise;
-        });
-      })
-    );
-	} else if (requestURL.pathname === BASE_PATH + 'staffs-uni.html') {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match('staffs-uni.html').then(function(cachedResponse) {
-          var fetchPromise = fetch('staffs-uni.html').then(function(networkResponse) {
-            cache.put('staffs-uni.html', networkResponse.clone());
-            return networkResponse;
-          });
-          return cachedResponse || fetchPromise;
-        });
-      })
-    );
- // Handle requests for Google Maps JavaScript API file
-  } else if (requestURL.href === googleMapsAPIJS) {
-    event.respondWith(
-      fetch(
-        googleMapsAPIJS+'&'+Date.now(),
-        { mode: 'no-cors', cache: 'no-store' }
-      ).catch(function() {
-        return caches.match('offline-map.js');
-      })
-    );
-	// Handle requests for events JSON file
-  } else if (requestURL.pathname === BASE_PATH + 'events.json') {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        }).catch(function() {
-          return caches.match(event.request);
-        });
-      })
-    );
-  } /*else if (requestURL.href === newsAPIJSON) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-          caches.delete(TEMP_IMAGE_CACHE_NAME);
-          return networkResponse;
-        }).catch(function() {
-          return caches.match(event.request);
-        });
-      })
-    );
-  // Handle requests for event images.
-  } */else if (requestURL.pathname.includes('/eventImages/')) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match(event.request).then(function(cacheResponse) {
-          return cacheResponse||fetch(event.request).then(function(networkResponse) {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          }).catch(function() {
-            return cache.match('eventImages/event-default.png');
-          });
-        });
-      })
-    );
-  // 
-  } else if (requestURL.href.includes('bbci.co.uk/news/')) {
-    event.respondWith(
-      caches.open(TEMP_IMAGE_CACHE_NAME).then(function(cache) {
-        return cache.match(event.request).then(function(cacheResponse) {
-          return cacheResponse||fetch(event.request, {mode: 'no-cors'}).then(function(networkResponse) {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          }).catch(function() {
-            return cache.match('eventImages/news-default.jpg');
-          });
-        });
-      })
-    );
-
-  
-  
-  
-  
-  } else if (
-    CACHED_URLS.includes(requestURL.href) ||
-    CACHED_URLS.includes(requestURL.pathname)
-  ) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-        });
-      })
-    );
-  }
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('index.html');
+        }
+      });
+    })
+  );
 });
-
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
@@ -395,8 +290,6 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
-
-
 
 
 
