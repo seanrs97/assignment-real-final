42e952d619da7c7429f86afc76ffe4c77dc47710
sean stevenson <sean stevenson>
2017-04-28 13:17:46 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -322,7 +322,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
     var requestURL = new URL(event.request.url);
     // Handle requests for index.html
-    if (requestURL.pathname === BASE_PATH + 'sign-up.html') { // THIS FAM
+    if (requestURL.pathname === BASE_PATH + 'sign-up.html') { // T
         event.respondWith(
             caches.open(CACHE_NAME).then(function(cache) {
                 return cache.match('sign-up.html').then(function(cachedResponse) {
