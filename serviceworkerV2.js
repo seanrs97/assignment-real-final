38c4564c0dd36ce9ca2f1d7fd04971c9470227af
sean stevenson <sean stevenson>
2017-04-28 12:59:17 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -322,21 +322,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
     var requestURL = new URL(event.request.url);
     // Handle requests for index.html
-if (requestURL.pathname === BASE_PATH + 'staffs-uni.html') {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(function(cache) {
-                return cache.match('staffs-uni.html').then(function(cachedResponse) {
-                    var fetchPromise = fetch('staffs-uni.html').then(function(networkResponse) {
-                        cache.put('staffs-uni.html', networkResponse.clone());
-                        return networkResponse;
-                    });
-                    return cachedResponse || fetchPromise;
-                });
-            })
-        );
-
-        // Handle requests for Google Maps JavaScript API file
-    } else if (requestURL.href === googleMapsAPIJS) {
+   if (requestURL.href === googleMapsAPIJS) {
         event.respondWith(
             fetch(
                 googleMapsAPIJS+'&'+Date.now(),
