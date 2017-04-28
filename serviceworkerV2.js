0623ade4884229c06b8b6c44d7f06a7d0fcf0085
sean stevenson <sean stevenson>
2017-04-28 11:52:12 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -328,15 +328,16 @@ self.addEventListener('fetch', function(event) {
         });
       })
     );
-  } else if ( CACHED_URLS.includes(requestURL.href) || CACHED_URLS.includes(requestURL.pathname) {
+  } else if (CACHED_URLS.includes(requestURL.href) || CACHED_URLS.includes(requestURL.pathname) {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match(event.request).then(function(response) {
           return response || fetch(event.request);
-        })
+        });
       })
     );
   });
+  
 });
 
 
