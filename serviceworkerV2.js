44f7412deb5ecd437ce3ff9b5a973ee19009f5d8
sean stevenson <sean stevenson>
2017-04-28 01:10:38 +0100

    new stuff

--- serviceworkerV2.js
+++ serviceworkerV2.js
@@ -282,8 +282,8 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
-       cacheNames.map(function(cacheName) {
-          if (CACHE_NAME !== cacheName && cacheName.startsWith('gih-cache')) {
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
             return caches.delete(cacheName);
           }
         })
@@ -291,7 +291,6 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
-
 
 
 
